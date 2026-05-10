36015e4b439ab1848e4a1afc3aa236fc06730b99
m-usama-z <m.usama.zubair978@gmail.com>
2026-05-10 03:47:50 -0500

    before adding density filter to plot_results.py

--- figures/figure_generation_commands.sh
+++ figures/figure_generation_commands.sh
@@ -1,5 +1,5 @@
 # figures for the paper
-python plot_results.py 31 34 --save_name figures/sys_mat_spec_dist_vs_gramian_spec_dist.png --system_matrix_type adjacency neg_laplacian --ylabel "System matrix spectral distance\nvs. Gramian spectral distance" --thresholds 0.8 0.95 --threshold_line_mode horizontal
-python plot_results.py 42 45 --save_name figures/density_vs_gramian_spec_dist.png --system_matrix_type adjacency neg_laplacian --ylabel "Density versus controllability\nGramian spectral distance" --thresholds 0.8 0.95 --threshold_line_mode horizontal
-python plot_results.py 26 --save_name figures/sys_mat_spec_dist_vs_density_linear_correlation.png --system_matrix_type adjacency neg_laplacian --ylabel "System matrix\nspectral distance\nversus density" --thresholds 0.8 0.95 --threshold_line_mode horizontal
+python plot_results.py 21 --save_name figures/sys_mat_spec_dist_vs_density_linear_correlation.png --system_matrix_type adjacency neg_laplacian --ylabel "System matrix\nspectral distance\nversus density" --thresholds 0.8 0.95 --threshold_line_mode horizontal --no_averaging --time_horizons 0.01 0.1 1 10
+python plot_results.py 25 27 --save_name figures/sys_mat_spec_dist_vs_gramian_spec_dist.png --system_matrix_type adjacency neg_laplacian --ylabel "System matrix spectral distance\nvs. Gramian spectral distance" --thresholds 0.8 0.95 --threshold_line_mode horizontal --no_averaging --time_horizons 0.01 0.1 1 10
+python plot_results.py 33 35 --save_name figures/density_vs_gramian_spec_dist.png --system_matrix_type adjacency neg_laplacian --ylabel "Density versus controllability\nGramian spectral distance" --thresholds 0.8 0.95 --threshold_line_mode horizontal --no_averaging --time_horizons 0.01 0.1 1 10
 
